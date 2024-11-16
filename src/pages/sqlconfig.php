57f95225027e2505efed16999e2cded6a3e8e000
Usaan <gabrielrebecacr@gmail.com>
2024-11-16 19:40:26 -0300

    corrigida atribuição de credenciais

--- src/pages/sqlconfig.php
+++ src/pages/sqlconfig.php
@@ -1,13 +1,12 @@
 <?php
     require_once('../../vendor/autoload.php');
-
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
     $dotenv->load();
 
-    $dbHost = $_ENV['aws-0-sa-east-1.pooler.supabase.com'];
-    $dbPort = $_ENV['6543'];
-    $dbName = $_ENV['postgres'];
-    $dbUser = $_ENV['postgres.yjqjikzgfvubiqswcbzz'];
-    $dbPassword = $_ENV['lilacringe123@'];
+    $dbHost = $_ENV['dbHost'];
+    $dbPort = $_ENV['dbPort'];
+    $dbName = $_ENV['dbName'];
+    $dbUser = $_ENV['dbUser'];
+    $dbPassword = $_ENV['dbPass'];
     $dsn = "pgsql:host=$dbHost;port=$dbPort;dbname=$dbName";
 ?>
